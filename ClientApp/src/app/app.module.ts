0baacd2d930d41896c7e50ff2c45d1f128b5fc4a
IQ <IQ@DESKTOP-MNR5VJF>
2022-01-28 02:11:51 +0600

    Add project files.

---
+++ ClientApp/src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { BranchFormComponent } from './components/branch-form/branch-form.component';
+import { BranchListComponent } from './components/branch-list/branch-list.component';
+
+import { BranchService } from './services/branch.service';
+import { HttpModule } from '@angular/http';
+/*import { CustomerService } from './services/customer.service';*/
+//import { CustomerFormComponent } from './components/customer-form/customer-form.component';
+//import { CustomerListComponent } from './components/customer-list/customer-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    CounterComponent,
+    FetchDataComponent,
+    BranchFormComponent,
+    BranchListComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+
+    HttpModule,
+
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'counter', component: CounterComponent },
+      { path: 'fetch-data', component: FetchDataComponent },
+
+      { path: 'branch/new', component: BranchFormComponent },
+      { path: 'branch/:id', component: BranchFormComponent },
+      { path: 'branch', component: BranchListComponent },
+
+      //{ path: 'customer/new', component: CustomerFormComponent },
+      //{ path: 'customer/:id', component: CustomerFormComponent },
+      //{ path: 'customer', component: CustomerListComponent },
+    ])
+  ],
+  providers: [BranchService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
